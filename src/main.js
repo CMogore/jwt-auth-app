75aecad793fb57805eebbe97324dafe0028f18d9
CMogore <chelsea.mogore@gmail.com>
2024-04-18 18:05:57 +0300

    jwt

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router'; // Import the router instance
+import store from "./store";
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+new Vue({
+  router, // Use the router instance
+  store,
+  render: h => h(App),
+}).$mount('#app');
